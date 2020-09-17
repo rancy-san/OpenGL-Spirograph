f42a00a4db41775c137a04e7ef1d43492831b170
Rancy <>
2020-09-17 12:56:48 -0700

    Create a project to visually create spirograph in OpenGL
    Create a visual spirograph in OpenGL using two circles and your keyboard.

---
+++ Readme.md
@@ -0,0 +1,40 @@
+## Spiro Graph - Draw a Spiro Function
+
+### Description
+Create a visual spirograph in OpenGL using two circles and your keyboard.
+
+### Compile Environment
+Linux
+
+### Dependencies
+sudo apt install make
+sudo apt-get install build-essential
+sudo apt-get install freeglut3-dev
+
+### Instructions
+In Terminal, cd to this project's 'Makefile' folder.
+
+### Compile in Terminal by typing
+make
+
+### Run in Terminal by typing
+./spiro
+
+### Usage (Press Keys)
+Draw a spirograph using keyboard inputs.
+
+
+a	Increase radius of center(static) circle
+A	Decrease radius of center(static) circle
+b	Increase radius of side circle
+B	Decrease radius of side circle
+h	Nudge offset to the left
+H	Nudge offset to the right
+r	Lower resolution
+R	Higher resolution
+i	Smaller spiro line
+I	Larger spiro line
+S	Display/Hide spiro
+D	Display/Hide Graph
+C	Activate/Deactivate Random Color
+M	Generate more spiro
---
+++ text.c
@@ -0,0 +1,36 @@
+/**************************************************************************/
+/* Include necessary header files
+ **************************************************************************/
+#include "text.h"	
+
+/**************************************************************************/
+/* Include necessary header files
+ **************************************************************************/
+GLint base = 0;
+
+
+/**************************************************************************/
+/* Function to create NUMCHARACTERS list, each of them with a character 
+ **************************************************************************/
+void CreateCharacters( void *font ) {
+int i;
+base = glGenLists( NUMCHARACTERS );
+
+for ( i = 0; i < NUMCHARACTERS; i++ ) {
+	glNewList( base + i, GL_COMPILE );
+		glutBitmapCharacter( font, i );
+	glEndList( );
+}
+
+}
+/**************************************************************************/
+/* Function to draw the text text
+ **************************************************************************/
+void Legend( int x, int y, char *text ) {
+
+	glRasterPos2f( x, y );
+        glListBase( base );
+        glCallLists( (GLint) strlen( text ), GL_BYTE, text );
+
+}
+
